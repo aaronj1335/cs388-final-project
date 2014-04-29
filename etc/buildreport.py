69937fc303b22e2e977f55101ba8ae627c0ab8f3
Aaron Stacy <aaron.r.stacy@gmail.com>
2014-04-29 11:49:14 -0500

    remove web-based documentation stuff

--- etc/buildreport.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-
-if __name__ == '__main__':
-    sys.stdout.write(open('etc/template.html').read() % {
-        'marked': open('etc/marked.js').read(),
-        'report': sys.stdin.read(),
-        })
